d3a4f29ed941843cfaf9aebb768b981d8c723d62
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2024-07-19 14:31:55 +0900

    feat: add c++ solution for leetcode 189

---
+++ leetcode/solutions/c++/a2/557a7b2e94197ff767970b67041697/189/solution.cc
@@ -0,0 +1,16 @@
+// 21 ms, 27.32 MB
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  void rotate(vector<int> &nums, int k) {
+    k = k % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+  }
+};
